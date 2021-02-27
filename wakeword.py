9a6c3ec4a759f73eee67d4d87c1697661d92960d
Ankithac45 <79328907+Ankithac45@users.noreply.github.com>
2021-02-27 15:52:03 +0530

    Update wakeword.py

--- wakeword.py
+++ wakeword.py
@@ -4,6 +4,9 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import webbrowser
+
+
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -11,9 +14,11 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+
 
 
 def take_command():
@@ -24,38 +29,43 @@ def take_command():
             command = listener.recognize_google(voice)
             command = command.lower()
             if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
+                return command
     except:
         pass
     return command
 
 
+
 def run_alexa():
     command = take_command()
-    print(command)
     if 'play' in command:
-        song = command.replace('play', '')
+        song = command
         talk('playing' + song)
         pywhatkit.playonyt(song)
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
+    elif 'who is' in command:
+        person = command.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
-    elif 'party' in command:
+    elif 'date' in command:
         talk('sorry, I am not interested')
     elif 'are you free now' in command:
         talk('Nope I am busy coding')
+    elif 'are you single' in command:
+        talk("I am in relationship with wifi")
     elif 'joke' in command:
         talk(pyjokes.get_joke())
-    elif 'are u single' in command:
-        talk('I am in relationship with wifi')
+    elif 'open facebook' in command:
+        talk('opening facebook')
+        webbrowser.open('https://www.facebook.com')
+    elif 'yourself' in command:
+        talk('I am Alexa,I am your digital assistant')
     else:
         talk('Please say the command again')
+
 
 
 while True:
